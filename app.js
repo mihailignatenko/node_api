d4d96747e68c32665945d39baf374cba8e95373a
AurumGallente <aurum.gallente@gmail.com>
2014-10-01 12:23:12 +0300

    added package.json

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 var connect_params = require('./connect');
 var express = require('express'),
+        url = require("url"),
+        swagger = require("swagger-node-express"),
         app = express(),
         mysql = require('mysql'),
         connectionpool = mysql.createPool(connect_params.connection);
+swagger.setAppHandler(app);
 //---------------------------------------------------------------------
-//Friends
-//var handleRequest = function(req, res) {
-//    try {
-//      res.writeHead(200);
-//
-//    } catch(e) {
-//      res.writeHead(200);
-//      res.end('Boo');
-//    }
-//};
+var where_field =1;
+var order_field = 1;
+var limit;
+var offset;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -22,7 +19,7 @@ app.use(function(req, res, next) {
 app.get('/profiles/:id/friends', function(req, res) {
     try {
     connectionpool.getConnection(function(err, connection) { 
-        connection.query('SELECT friends.* FROM Profiles as friends INNER JOIN sys_friend_list as sys ON sys.Profile = friends.id INNER JOIN Profiles as p ON sys.id = p.id WHERE p.id = '+req.params.id, req.params.id, function(err, rows, fields) {
+        connection.query('SELECT  friends.`ID`, friends.`NickName`, friends.`Email`, friends.`Status`, friends.`Role`, friends.`Couple`, friends.`Sex`, friends.`LookingFor`, friends.`Headline`, friends.`DescriptionMe`, friends.`Country`, friends.`City`, friends.`DateOfBirth`, friends.`Featured`, friends.`DateReg`, friends.`DateLastEdit`, friends.`DateLastLogin`, friends.`DateLastNav`, friends.`aff_num`, friends.`Tags`, friends.`zip`, friends.`EmailNotify`, friends.`LangID`, friends.`UpdateMatch`, friends.`Views`, friends.`Rate`, friends.`RateCount`, friends.`CommentsCount`, friends.`PrivacyDefaultGroup`, friends.`allow_view_to`, friends.`UserStatus`, friends.`UserStatusMessage`, friends.`UserStatusMessageWhen`, friends.`Avatar`, friends.`Height`, friends.`Weight`, friends.`Income`, friends.`Occupation`, friends.`Religion`, friends.`Education`, friends.`RelationshipStatus`, friends.`Hobbies`, friends.`Interests`, friends.`Ethnicity`, friends.`FavoriteSites`, friends.`FavoriteMusic`, friends.`FavoriteFilms`, friends.`FavoriteBooks`, friends.`FirstName`, friends.`LastName`, friends.`FacebookProfile` FROM Profiles as friends INNER JOIN sys_friend_list as sys ON sys.Profile = friends.id INNER JOIN Profiles as p ON sys.id = p.id WHERE p.id = '+req.params.id, req.params.id, function(err, rows, fields) {
             res.send({
                 result: 'success',
                 err: '',
@@ -117,11 +114,29 @@ app.delete('/:table/:id', function(req, res) {
 });
 
 //------------------------------------------------------------------------------
-// Profiles
+// Profiles/:id
 app.get('/profiles/:id', function(req, res) {
     try {
     connectionpool.getConnection(function(err, connection) { 
-        connection.query('SELECT * FROM Profiles WHERE id = '+req.params.id, req.params.id, function(err, rows, fields) {
+        connection.query('SELECT `ID`, `NickName`, `Email`, `Status`, `Role`, `Couple`, `Sex`, `LookingFor`, `Headline`, `DescriptionMe`, `Country`, `City`, `DateOfBirth`, `Featured`, `DateReg`, `DateLastEdit`, `DateLastLogin`, `DateLastNav`, `aff_num`, `Tags`, `zip`, `EmailNotify`, `LangID`, `UpdateMatch`, `Views`, `Rate`, `RateCount`, `CommentsCount`, `PrivacyDefaultGroup`, `allow_view_to`, `UserStatus`, `UserStatusMessage`, `UserStatusMessageWhen`, `Avatar`, `Height`, `Weight`, `Income`, `Occupation`, `Religion`, `Education`, `RelationshipStatus`, `Hobbies`, `Interests`, `Ethnicity`, `FavoriteSites`, `FavoriteMusic`, `FavoriteFilms`, `FavoriteBooks`, `FirstName`, `LastName`, `FacebookProfile` FROM Profiles WHERE id = '+req.params.id, req.params.id, function(err, rows, fields) {
+            res.send({
+                result: 'success',
+                err: '',
+                //fields: fields,
+                json: rows
+                //length: rows.length
+            });
+            connection.release();
+        });
+    });
+    } catch(e) {
+        res.writeHead(err);
+    }    
+});
+app.get('/profiles', function(req, res) {
+    try {
+    connectionpool.getConnection(function(err, connection) { 
+        connection.query('SELECT `ID`, `NickName`, `Email`, `Status`, `Role`, `Couple`, `Sex`, `LookingFor`, `Headline`, `DescriptionMe`, `Country`, `City`, `DateOfBirth`, `Featured`, `DateReg`, `DateLastEdit`, `DateLastLogin`, `DateLastNav`, `aff_num`, `Tags`, `zip`, `EmailNotify`, `LangID`, `UpdateMatch`, `Views`, `Rate`, `RateCount`, `CommentsCount`, `PrivacyDefaultGroup`, `allow_view_to`, `UserStatus`, `UserStatusMessage`, `UserStatusMessageWhen`, `Avatar`, `Height`, `Weight`, `Income`, `Occupation`, `Religion`, `Education`, `RelationshipStatus`, `Hobbies`, `Interests`, `Ethnicity`, `FavoriteSites`, `FavoriteMusic`, `FavoriteFilms`, `FavoriteBooks`, `FirstName`, `LastName`, `FacebookProfile` FROM Profiles', req.params.id, function(err, rows, fields) {
             res.send({
                 result: 'success',
                 err: '',
