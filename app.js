0129ad171718233039496c76039bedd1cf428750
AurumGallente <aurum.gallente@gmail.com>
2014-10-16 15:41:04 +0300

    fix sql query for register#3

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/register', function(request, response){
         console.log(sql);        
             try {
             connectionpool.getConnection(function(err, connection, sql) { 
-                connection.query("SELECT count(id) as count FROM Profiles WHERE NickName = "+mysql.escape(request.body.NickName), request.body.NickName, function(err, rows, fields, sql) {
+                connection.query("SELECT count(id) as count FROM Profiles WHERE NickName = '"+mysql.escape(request.body.NickName)+"'", request.body.NickName, function(err, rows, fields, sql) {
                     console.log(rows);
                     count = rows[0].count;
                     console.log(count);
